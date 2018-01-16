fe1b60a6c33ee014c8a81ccb2271188034895942
johnnason <john.nason@gmail.com>
2018-01-16 11:16:50 -0500

    clean up commit message format

--- ci/scripts/last-commit.sh
+++ ci/scripts/last-commit.sh
@@ -2,4 +2,5 @@
 
 cd ci-assets
 # TODO: paramaterize file output
-git --no-pager log -n 1 > ../last-commit-message/last-commit.txt
+# Format: <name>: <commit hash>, <time> \n <message>
+git --no-pager log -n 1 --format="# %an: %H, %ai%n%n%s%n" ../last-commit-message/last-commit.txt
